4249315bb75bd100b4a66327afb197af1bed3aa1
Alexis Tucker <tuckera6@oregonstate.edu>
2019-05-06 08:33:54 -0700

    added README and testme.c

---
+++ projects/tuckera6/quiz/README.txt
@@ -0,0 +1,5 @@
+***README.TXT***
+
+1. To compile and execute testme run: make testme
+2. To view the coverage results run: make results
+NOTE: "make results" must be run after "make testme"
---
+++ projects/tuckera6/quiz/testme.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+	//generate ASCII code from 32(space) to 126(~) and return it as a character
+    return rand() % (126 - 32) + 32;
+}
+
+char *inputString()
+{
+	//decide if ranString should be "reset" (1 in 1000000 chance)
+	if (rand()%(1000000 - 1) + 1 == 500000) {
+		char* resetString = "reset\0";
+		return resetString;
+	}
+	else {
+		//generate random string length from 10 to 20
+		int length = rand() % (20 - 10) + 1;
+
+		//generate a random character the length of the string
+		int i;
+		char* ranString = malloc(sizeof(char) * (length + 1));
+		memset(ranString, '\0', sizeof(ranString));
+		for (i = 0; i < length; i++) {
+			char temp = rand() % (126 - 32) + 32;
+			ranString[i] = temp;
+		}
+		ranString[length] = '\0';	//add null terminator to end of string
+		return ranString;			//return random, null-terminated string
+	}
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
